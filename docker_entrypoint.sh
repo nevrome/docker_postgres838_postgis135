c680ed242567e6532672dcd6f0d99bb2c1bb0a85
Clemens Schmid <clemens@nevrome.de>
2018-06-25 16:58:43 +0200

    container should not stop if db user already exists

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -8,7 +8,7 @@ su - postgres -c "
 export PATH=$PATH:/usr/local/pgsql/bin &&\
 echo \"export PATH=$PATH:/usr/local/pgsql/bin\" >> /home/postgres/.bashrc &&\
 pg_ctl -w -D /usr/local/pgsql/data -l /usr/local/pgsql/data/logfile start &&\
-psql -h 127.0.0.1 --command \"CREATE USER $POSTGRES_USER WITH SUPERUSER PASSWORD $POSTGRES_PASSWORD;\"
+if psql -t -c '\du' | cut -d \| -f 1 | grep -qw \"$POSTGRES_USER\"; then echo \"main db user already exists\"; else psql -h 127.0.0.1 --command \"CREATE USER $POSTGRES_USER WITH SUPERUSER PASSWORD $POSTGRES_PASSWORD;\"; fi
 "
 
 exec "$@";
